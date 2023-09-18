e9b3f292040641ac5b0909f096e10223e66a2e1b
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-09-18 23:09:24 +0200

    Cambios para la siguiente clase

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+// Función para generar un color aleatorio en formato hexadecimal
+function generateRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+// Función para cambiar el color de fondo del body
+function changeBackgroundColor() {
+    const newColor = generateRandomColor();
+    document.body.style.backgroundColor = newColor;
+}
+
+// Agregar un evento al botón para cambiar el color al hacer clic
+const changeColorBtn = document.getElementById("changeColorBtn");
+changeColorBtn.addEventListener("click", changeBackgroundColor);
