98dd158182de2da6beecf8b6a3225dedd9fb90e3
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-09-19 12:33:49 +0200

    Añadimos arrow functions al index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 // Función para generar un color aleatorio en formato hexadecimal
-function generateRandomColor() {
+const generateRandomColor = color => {
     const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
     console.log(color)
     return color
 }
 
 // Función para cambiar el color de fondo del body
-function changeBackgroundColor() {
+const changeBackgroundColor = () => {
     const newColor = generateRandomColor();
     document.body.style.backgroundColor = newColor;
 }
