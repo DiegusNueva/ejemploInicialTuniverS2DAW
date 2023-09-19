95d9772082eaa69e82ab6aa4ae858f630f57340a
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-09-19 12:12:49 +0200

    Cambio del index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 // Función para generar un color aleatorio en formato hexadecimal
 function generateRandomColor() {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    console.log(color)
+    return color
 }
 
 // Función para cambiar el color de fondo del body
