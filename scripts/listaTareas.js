5f3b8865ca73ac2187a662fcf91568632dbd4473
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-09-25 19:33:48 +0200

    Comenzamos a introducir comentarios y mejoramos el código

--- scripts/listaTareas.js
+++ scripts/listaTareas.js
@@ -1,4 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener -> es una función de JavaScript que se utiliza para escuchar eventos en una página web. En otras palabras, te permite decirle al navegador que esté atento a algo específico que pueda ocurrir en la página y que ejecute cierto código cuando ese evento suceda.
+
+// DOMContentLoaded: Es una forma de garantizar que tu código JavaScript se ejecute solo después de que la página web haya terminado de cargarse, evitando problemas de interacción con elementos que aún no han aparecido en la pantalla.
+
+document.addEventListener("DOMContentLoaded", () => {
     const taskInput = document.getElementById("task-input");
     const addTaskButton = document.getElementById("add-task");
     const taskList = document.getElementById("task-list");
